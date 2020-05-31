74b8d2d42ba0653360aba053c93227aea2c749fb
Hanz <firhan.hardiansyah09@gmail.com>
2020-05-31 14:53:19 +0700

    menambahkan beberapa konfig untuk di deploy

--- function/koneksi.php
+++ function/koneksi.php
@@ -5,4 +5,9 @@
   $pass = "";
   $db   = "db_book_store";
 
+  // $host = "ftp.deadliners.my.id";
+  // $user = "deadline";
+  // $pass = "28desember1969";
+  // $db   = "deadline_get_the_book";
+
   $con = mysqli_connect($host, $user, $pass, $db) or die("Koneksi Gagal");
