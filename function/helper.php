8d7e13c809e6301d7319b8976a2e514d381e9343
Hanz <firhan.hardiansyah09@gmail.com>
2020-05-31 21:19:09 +0700

    comment local

--- function/helper.php
+++ function/helper.php
@@ -1,7 +1,7 @@
 <?php
 
-define("BASE_URL", "http://localhost/book-store/");
-// define("BASE_URL", "https://deadliners.my.id/");
+// define("BASE_URL", "http://localhost/book-store/");
+define("BASE_URL", "https://deadliners.my.id/");
 
 function rupiah($nilai = 0)
 {
