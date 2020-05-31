f975fa340f8ffae092ae197db69d082ea52648d4
Hanz <firhan.hardiansyah09@gmail.com>
2020-05-31 14:54:18 +0700

    menambahkan form konfirm pembayaran

---
+++ module/pesanan/konfirmasi_pembayaran.php
@@ -0,0 +1,43 @@
+<?php
+
+  $pesanan_id = $_GET['pesanan_id']
+?>
+
+<div class="container pt-4">
+  <div class="card card-shadow mb-4">
+    <div class="card-header">
+      <h5>Konfirmasi Pembayaran</h5>
+    </div>
+    <div class="card-body">
+      <form action="<?= BASE_URL ?>module/pesanan/action.php?pesanan_id=<?= $pesanan_id ?>" method="POST">
+        <div class="form-group">
+          <label>No Rekening</label>
+          <input type="text" name="nomor_rekening" class="form-control" autocomplete="off">
+        </div>
+        <div class="form-group">
+          <label>Nama Account</label>
+          <input type="text" name="nama_account" class="form-control" autocomplete="off">
+        </div>
+        <div class="form-group">
+          <label>Tanggal Transfer</label>
+          <input type="date" name="tanggal_transfer" class="form-control" autocomplete="off">
+        </div>
+        <div class="mt-4 row">
+          <div class="col col-xl-6 col-lg-6 col-sm-6">
+            <a href="<?= BASE_URL ?>index.php?page=my_profile&module=pesanan&action=detail&pesanan_id=<?= $pesanan_id ?>" class="btn btn-primary mr-3 card-shadow mb-4 col-xl-4 col-lg-4 col-md-6"
+              style="border-radius: 30px; padding: 10px; width: 100%;">
+              <i class="fas fa-chevron-left"></i> Cek Kembali
+            </a>
+          </div>
+          <div class="col col-xl-6 col-lg-6 col-sm-6">
+            <a href="<?= BASE_URL ?>index.php?page=data_pemesanan"
+              class="btn btn-success float-right card-shadow col-xl-4 col-lg-4 col-md-6"
+              style="border-radius: 30px; padding: 10px; width: 100%;">
+              Pembayaran <i class="fas fa-chevron-right"></i>
+            </a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
