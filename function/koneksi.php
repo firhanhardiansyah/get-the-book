fb2ac7228741a9ad67d8d89677415e65da30dd44
Hanz <firhan.hardiansyah09@gmail.com>
2020-05-31 21:34:29 +0700

    pake yg local aja

--- function/koneksi.php
+++ function/koneksi.php
@@ -1,13 +1,8 @@
 <?php
 
-  // $host = "localhost";
-  // $user = "root";
-  // $pass = "";
-  // $db   = "db_book_store";
-
-  $host = "ftp.deadliners.my.id";
-  $user = "deadline";
-  $pass = "28desember1969";
-  $db   = "deadline_get_the_book";
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db   = "db_book_store";
 
   $con = mysqli_connect($host, $user, $pass, $db) or die("Koneksi Gagal");
