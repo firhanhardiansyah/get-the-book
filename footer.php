2a14790a7c011f30c0938a262837c40bba280f58
Hanz <firhan.hardiansyah09@gmail.com>
2020-06-07 14:01:33 +0700

    add tentang

--- footer.php
+++ footer.php
@@ -1,16 +1,11 @@
 <!-- Footer -->
-<footer class="py-4 bg-light mt-auto" style="">
-  <div class="container-fluid">
-    <div class="d-flex align-items-center justify-content-between small">
-      <div class="text-muted">
-        Copyright &copy; Firhan Hardiansyah |
-        Build : Selasa, ‎05 ‎Mei ‎2020, ‏‎05.40.55
-      </div>
-      <div>
-        <a href="#">Tentang Apilkasi</a>
-        &middot;
-      </div>
-    </div>
+<footer class="page-footer font-small bg-electric-violet text-white">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright: Firhan Hardiansyah | 
+    <a href="index.php?page=tentang" class="text-white"> Tentang Aplikasi</a>
   </div>
+  <!-- Copyright -->
+
 </footer>
-<!-- End Footer -->
+<!-- Footer -->
---
+++ tentang.php
@@ -0,0 +1,46 @@
+<div class="container pt-4">
+  <h2>Tentang Aplikasi</h2>
+  <h5>Aplikasi ini dibuat adalah untuk memenuhi tugas akhir dari mata kuliah RPL (Rancangan Perangkat Lunak) </h5>
+  <hr>
+  <h2>Nama Anggota</h2>
+  <div class="row">
+    <div class="col-4 mt-4">
+      <div class="card">
+        <img class="mb-4 card-shadow card-img-top"
+          src="<?= BASE_URL ?>assets/img/tentang/saya.jpeg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="text-secondary">Front End & Back End</h5>
+        <h4>Firhan Hardiansyah</h4>
+        <h5>1810631170184</h5>
+        </div>
+      </div>
+    </div>
+    <div class="col-4 mt-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">s</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-4 mt-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">s</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-4 mt-4">
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="">
+  <?php
+  require_once("footer.php");
+  ?>
+</div>
