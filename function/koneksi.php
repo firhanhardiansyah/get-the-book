d579950ccee63c14042beb5cf9ee8f683bc85e92
Hanz <firhan.hardiansyah09@gmail.com>
2020-06-07 20:08:26 +0700

    edit

--- function/koneksi.php
+++ function/koneksi.php
@@ -2,7 +2,7 @@
 
   $host = "ftp.getthebook.my.id";
   $user = "getq5835";
-  $pass = "WjM2Q~2R2FR[lrf0";
-  $db   = "id13988189_get_the_book";
+  $pass = "showmesecret01";
+  $db   = "getq5835_get_the_book";
 
   $con = mysqli_connect($host, $user, $pass, $db) or die("Koneksi Gagal");
