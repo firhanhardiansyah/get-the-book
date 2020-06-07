eda188f2b420ccb5dbab2944efeadc7f223b287c
Hanz <firhan.hardiansyah09@gmail.com>
2020-06-07 14:43:54 +0700

    edit dikit

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <footer class="page-footer font-small bg-electric-violet text-white">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright: Firhan Hardiansyah | 
+  <div class="footer-copyright text-center py-3">© 2020 Copyright: Perwakilan Kelompok, Firhan Hardiansyah | 
     <a href="index.php?page=tentang" class="text-white"> Tentang Aplikasi</a>
   </div>
   <!-- Copyright -->
