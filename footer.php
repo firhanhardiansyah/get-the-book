ba2272f9f4cff5d93208de077468ca4fdc6b9ddc
Hanz <firhan.hardiansyah09@gmail.com>
2020-06-09 21:44:03 +0700

    edit dikit

--- footer.php
+++ footer.php
@@ -1,16 +1,12 @@
 <!-- Footer -->
-<footer class="py-4 bg-light mt-auto" style="">
-  <div class="container-fluid">
-    <div class="d-flex align-items-center justify-content-between small">
-      <div class="text-muted">
-        Copyright &copy; Firhan Hardiansyah |
-        Build : Selasa, ‎05 ‎Mei ‎2020, ‏‎05.40.55
-      </div>
-      <div>
-        <a href="#">Tentang Apilkasi</a>
-        &middot;
-      </div>
-    </div>
+<footer class="page-footer font-small bg-electric-violet text-white" style="margin-top: 70px;">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+    Copyright &copy; Firhan Hardiansyah |
+    Build : Selasa, ‎05 ‎Mei ‎2020, ‏‎05.40.55
   </div>
+  <!-- Copyright -->
+
 </footer>
-<!-- End Footer -->
+<!-- Footer -->
