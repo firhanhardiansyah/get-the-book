8326dba38ff2733aa7a6bf00cf95d7bf2f73cf8b
Hanz <firhan.hardiansyah09@gmail.com>
2020-06-10 02:01:52 +0700

    Memperbaiki QTY menjadi lebih baik

--- keranjang.php
+++ keranjang.php
@@ -1,7 +1,4 @@
-<?php
-
-if ($total_buku == 0) {
-?>
+<?php if ($total_buku == 0) { ?>
 <div class="container mt-3">
   <h1>Opss...</h1>
   <h1>Belum ada belanjaan.</h1>
@@ -19,36 +16,42 @@ if ($total_buku == 0) {
       <div class="table-responsive">
         <table class="table table-hover">
           <?php
-        $sub_total = 0;
-        if (is_array($keranjang) || is_object($keranjang)) :
-          foreach ($keranjang as $key => $value) :
-            $buku_id   = $key;
+            $sub_total = 0;
+            if (is_array($keranjang) || is_object($keranjang)) :
+              foreach ($keranjang as $key => $value) :
+                $buku_id   = $key;
 
-            $gambar    = $value['gambar'];
-            $nama_buku = $value['nama_buku'];
-            $penulis   = $value['penulis'];
-            $quantity  = $value['quantity'];
-            $harga     = $value['harga'];
+                $gambar    = $value['gambar'];
+                $nama_buku = $value['nama_buku'];
+                $penulis   = $value['penulis'];
+                $quantity  = $value['quantity'];
+                $harga     = $value['harga'];
 
-            $total = $quantity * $harga;
-            $sub_total += $total;
-        ?>
+                $total = $quantity * $harga;
+                $sub_total += $total;
+            ?>
           <tr>
             <td>
               <div class="row">
                 <div class="col-lg-2 col-md-2 text-center mb-3">
                   <img src="<?= BASE_URL ?>assets/img/buku/<?= $gambar ?>" alt="" style="height: 100px">
                 </div>
-                <div class="col-lg-10 col-md-10 align-self-center">
+                <div class="col-lg-10 col-md-10 mt-2">
                   <h5 class=""><?= $nama_buku ?></h5>
                   <h6 class="text-secondary"><?= $penulis ?></h6>
                 </div>
               </div>
             </td>
+            <!-- Qty -->
             <td class="text-center">
-              <input type="text" class="form-group text-center update-quantity" name="<?= $buku_id ?>"
-                value="<?= $quantity ?>" style="width: 50px" id="">
+              <div class="btn btn-secondary btn-sm" onclick="decrement_quantity(<?= $buku_id ?>)"
+                style="margin-top: -5px; width: 30px;">-</div>
+              <input type="text" class="form-group text-center update-quantity mt-1" name="<?= $buku_id ?>"
+                value="<?= $quantity ?>" style="width: 50px" id="input-quantity-<?= $buku_id ?>" disabled>
+              <div class="btn btn-secondary btn-sm" onclick="increment_quantity(<?= $buku_id ?>)"
+                style="margin-top: -5px; width: 30px;">+</div>
             </td>
+            <!-- End Qty -->
             <td class="text-center"><?= rupiah($harga) ?></td>
             <td class="text-center"><?= rupiah($total) ?></td>
             <td class="text-center">
@@ -58,10 +61,10 @@ if ($total_buku == 0) {
             </td>
           </tr>
           <?php
-            $no++;
-          endforeach;
-        endif;
-        ?>
+                $no++;
+              endforeach;
+            endif;
+            ?>
           <tfoot>
             <tr>
               <th>Sub Total</th>
@@ -84,7 +87,8 @@ if ($total_buku == 0) {
         </a>
       </div>
       <div class="col col-xl-6 col-lg-6 col-sm-6">
-        <a href="<?= BASE_URL ?>index.php?page=data_pemesanan" class="btn btn-success float-right card-shadow col-xl-4 col-lg-4 col-md-6"
+        <a href="<?= BASE_URL ?>index.php?page=data_pemesanan"
+          class="btn btn-success float-right card-shadow col-xl-4 col-lg-4 col-md-6"
           style="border-radius: 30px; padding: 10px; width: 100%;">
           Pembayaran <i class="fas fa-chevron-right"></i>
         </a>
@@ -116,4 +120,38 @@ if ($total_buku == 0) {
         }
       });
   });
+
+  function decrement_quantity(buku_id) {
+    var inputQuantityElement = $("#input-quantity-" + buku_id);
+    if ($(inputQuantityElement).val() > 1) {
+      var newQuantity = parseInt($(inputQuantityElement).val()) - 1;
+      save_to_db(buku_id, newQuantity);
+    }
+  }
+
+  function increment_quantity(buku_id) {
+    var inputQuantityElement = $("#input-quantity-" + buku_id);
+    var newQuantity = parseInt($(inputQuantityElement).val()) + 1;
+    save_to_db(buku_id, newQuantity);
+  }
+
+  function save_to_db(buku_id, value) {
+    var inputQuantityElement = $("#input-quantity-" + buku_id);
+    $.ajax({
+        url: "update_keranjang.php",
+        data: "buku_id=" + buku_id + "&value=" + value,
+        type: 'post',
+        success: function (response) {
+          $(inputQuantityElement).val(value);
+        }
+      })
+      .done(function (data) {
+        var json = $.parseJSON(data);
+        if (json.status == true) {
+          location.reload();
+        } else {
+          alert(json.pesan);
+        }
+      });
+  }
 </script>
--- update_keranjang.php
+++ update_keranjang.php
@@ -1,34 +1,32 @@
 <?php
 
-  include_once("function/koneksi.php");
+include_once("function/koneksi.php");
 
-  $buku_id   = $_POST["buku_id"];
-  $value     = $_POST["value"];
-  $status    = false;
-  $pesan     = '';
+$buku_id   = $_POST["buku_id"];
+$value     = $_POST["value"];
+$status    = false;
+$pesan     = '';
 
-  $query = mysqli_query($con, "SELECT stok FROM buku WHERE buku_id='$buku_id'");
-  $row = mysqli_fetch_array($query);
+$query = mysqli_query($con, "SELECT stok FROM buku WHERE buku_id='$buku_id'");
+$row = mysqli_fetch_array($query);
 
-  if ($row['stok'] >= $value) {
-    $status = true;
+if ($row['stok'] >= $value) {
+  $status = true;
 
-    session_start();
-    $keranjang = $_SESSION["keranjang"];
-    $keranjang[$buku_id]["quantity"] = $value;
-    $_SESSION["keranjang"] = $keranjang;
-  } else {
-    $status = false;
-    $pesan  = "Stok Tersisa " . $row['stok'];
-  }
+  session_start();
+  $keranjang = $_SESSION["keranjang"];
+  $keranjang[$buku_id]["quantity"] = $value;
+  $_SESSION["keranjang"] = $keranjang;
+} else {
+  $status = false;
+  $pesan  = "Stok Tersisa " . $row['stok'];
+}
 
-  $arr = [
-    'status' => $status,
-    'pesan' => $pesan
-  ];
+$arr = [
+  'status' => $status,
+  'pesan' => $pesan
+];
 
-  $json = json_encode($arr);
+$json = json_encode($arr);
 
-  echo $json;
-
-  
+echo $json;
