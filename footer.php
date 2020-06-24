eaf7526bf8989df78e206c6fb04a44d461824335
Hanz <firhan.hardiansyah09@gmail.com>
2020-06-25 02:34:33 +0700

    Edit Tentang

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <!-- Footer -->
-<footer class="page-footer font-small bg-electric-violet text-white" style="margin-top: 70px;">
+<footer class="page-footer font-small bg-electric-violet text-white">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">
-    Copyright &copy; SITEKTIF
+  <div class="footer-copyright text-center py-3">© 2020 Copyright: SITEKTIF | 
+    <a href="index.php?page=tentang" class="text-white"> Tentang Aplikasi</a>
   </div>
   <!-- Copyright -->
 
